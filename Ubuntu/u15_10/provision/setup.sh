e7a93601d50a2284adfa5dbe6180b749649c0a4c
Gabriel Perdue <perdue@fnal.gov>
2016-01-21 15:24:15 -0600

    install x11 and try to build GENIE

--- Ubuntu/u15_10/provision/setup.sh
+++ Ubuntu/u15_10/provision/setup.sh
@@ -11,3 +11,10 @@ sudo apt-get install subversion -y > /dev/null
 echo "Installing Subversion"
 sudo apt-get install autoconf -y > /dev/null
 
+echo "Installing libX11"
+sudo apt-get install libx11-dev -y > /dev/null
+
+echo "Installing GENIE"
+git clone https://github.com/GENIEMC/lamp.git
+pushd lamp
+./rub_the_lamp.sh -s
