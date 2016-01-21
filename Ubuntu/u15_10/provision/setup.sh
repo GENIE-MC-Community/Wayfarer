0156d64f73ba63c4c392ff75596867d55f01d0b0
Gabriel Perdue <perdue@fnal.gov>
2016-01-20 18:09:56 -0600

    ubuntu 15.10

---
+++ Ubuntu/u15_10/provision/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+
+echo "Installing Git"
+sudo apt-get install git -y > /dev/null
+
+echo "Installing Subversion"
+sudo apt-get install subversion -y > /dev/null
+
+echo "Installing Subversion"
+sudo apt-get install autoconf -y > /dev/null
+
