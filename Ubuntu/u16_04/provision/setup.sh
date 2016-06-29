b37a1d0cdf710ed4a9da5048e683fe6739fa2168
Gabriel Perdue <perdue@fnal.gov>
2016-06-29 13:09:33 -0500

    add ubuntu 16.04

---
+++ Ubuntu/u16_04/provision/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# make `debconf` use a frontend that exepcts no interactive input
+#  http://serverfault.com/questions/500764/dpkg-reconfigure-unable-to-re-open-stdin-no-file-or-directory
+sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
+sudo dpkg-reconfigure debconf -f noninteractive -p critical
+
+echo "Provisioning virtual machine..."
+
+echo "Updating apt-get"
+sudo apt-get update > /dev/null
+
+echo "Installing Git"
+sudo apt-get install git -y > /dev/null
+
+echo "Installing Subversion"
+sudo apt-get install subversion -y > /dev/null
+
+echo "Installing Python"
+sudo apt-get install python -y > /dev/null
+
+echo "Installing autoconf"
+sudo apt-get install autoconf -y > /dev/null
+
+echo "Installing libX11"
+sudo apt-get install libx11-dev -y > /dev/null
+
+echo "Installing libXpm"
+sudo apt-get install libxpm-dev -y > /dev/null
+
+echo "Installing libXft"
+sudo apt-get install libxft-dev -y > /dev/null
+
+echo "Installing libXext"
+sudo apt-get install libxext-dev -y > /dev/null
+
+echo "Installing libpng"
+sudo apt-get install libpng-dev -y > /dev/null
+
+echo "Installing libjpeg"
+sudo apt-get install libjpeg-dev -y > /dev/null
+
+echo "Installing libtool"
+sudo apt-get install libtool -y > /dev/null
+
+echo "Installing gfortran"
+sudo apt-get install gfortran -y > /dev/null
+
+echo "Installing libxml2-dev"
+sudo apt-get install libxml2-dev -y > /dev/null
+
+echo "Installing gdb because reasons"
+sudo apt-get install gdb -y > /dev/null
+
+echo "Installing GENIE"
+git clone https://github.com/GENIEMC/lamp.git
+pushd lamp
+./rub_the_lamp.sh -s --root v5-34-30 --support-tag R-2_10_6.0 --no-roomu
